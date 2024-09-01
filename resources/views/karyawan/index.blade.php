1af34f42c19ab9dffe764915b3c972f1cfc998ca
mahdiawan-nk <dev.mrcoders@gmail.com>
2024-09-01 12:58:08 +0700

    update fitur data controls

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -9,7 +9,7 @@
     <div class="page-wrapper">
         <div class="page-content">
             <!--breadcrumb-->
-            <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="page-breadcrumb align-items-center mb-3">
                 <div class="breadcrumb-title pe-3">Data Master</div>
                 <div class="ps-3">
                     <nav aria-label="breadcrumb">
